a665aa8864ff696c25c35cf157092db392f6b340
ww33 <yinfo333@gmail.com>
2023-02-26 07:07:20 +0300

    bybit-api work

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -6,6 +6,7 @@ import { isBrowser } from '@/utils/ssr';
 Axios.interceptors.request.use(
   (config: AxiosRequestConfig) => {
     const isExternal = !!config?.url?.startsWith('http');
+    console.log({isExternal});
     const token = isBrowser ? localStorage.getItem(AUTH_TOKEN_KEY) : null;
     const authHeaders =
       token && !isExternal ? { Authorization: `Bearer ${token}` } : {};
